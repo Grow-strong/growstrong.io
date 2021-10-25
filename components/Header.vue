eb3d0fda746121b7b0870da64d5ae65e5591845c
nickbahson <nijoba33@gmail.com>
2021-10-25 12:38:12 +0300

    initial scaffolds for grow_strong website

---
+++ components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <header>
+    <h1 class="title"> Grow Strong! </h1>
+    <b-navbar>
+      <template #start>
+        <b-navbar-item href="#">
+          Home
+        </b-navbar-item>
+        <b-navbar-item href="#">
+          Documentation
+        </b-navbar-item>
+        <b-navbar-dropdown label="Info">
+          <b-navbar-item href="#">
+            About
+          </b-navbar-item>
+          <b-navbar-item href="#">
+            Contact
+          </b-navbar-item>
+        </b-navbar-dropdown>
+      </template>
+
+    </b-navbar>
+    <section>
+      <b-field>
+        <b-switch v-model="isDarkMode"
+                  true-value="Light"
+                  false-value="Dark">
+          {{ isDarkMode }}
+        </b-switch>
+      </b-field>
+      <b-field>
+        <b-switch
+          v-model="lightMode"
+          passive-type='is-dark'
+          type='is-warning'
+          @change="changeColorMode($event)">
+          {{ lightMode ? "Light Mode" : "Dark Mode" }}
+        </b-switch>
+      </b-field>
+    </section>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      isDarkMode: 'Light',
+      lightMode: false
+    }
+  },
+  watch: {
+    lightMode: function () {
+      this.$emit('light-mode', this.lightMode)
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <!-- <nav
+      class="navbar header has-shadow is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a
+          class="navbar-item"
+          href="/"
+        >
+          <img
+            src="~assets/buefy.png"
+            alt="Buefy"
+            height="28"
+          >
+        </a>
+
+        <div class="navbar-burger">
+          <span />
+          <span />
+          <span />
+        </div>
+      </div>
+    </nav> -->
+
+    <section :class="this.viewMode">
+      <!--<aside class="column is-2 section">
+        <p class="menu-label is-hidden-touch">
+          General
+        </p>
+        <ul class="menu-list">
+          <li
+            v-for="(item, key) of items"
+            :key="key"
+          >
+            <NuxtLink
+              :to="item.to"
+              exact-active-class="is-active"
+            >
+              <b-icon :icon="item.icon" /> {{ item.title }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </aside>-->
+
+      <div class="container column is-10">
+        <Header v-on:light-mode="updateViewMode"/>
+        <Nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header";
+export default {
+  data() {
+    return {
+      viewMode: false
+    }
+  },
+  components: {
+    Header
+  },
+  methods: {
+    updateViewMode(mode) {
+      console.log('+++++++++++++++++ MODE ++++++++++++++++++')
+      console.log(mode)
+      console.log('+++++++++++++++++ MODE ++++++++++++++++++')
+      this.viewMode = mode ? 'light' : 'dark'
+    }
+  }
+  /*data () {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }*/
+}
+</script>
